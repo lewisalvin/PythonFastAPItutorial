3e9ffaa56bb6ebd1688203d3cebf918fbdd868e6
Alvin <lewiscreated@gmail.com>
2025-03-05 21:27:38 -0500

    small update made to myapi.py file.  Added new endpoint to accept dynamic patient_id with validation conditions.

--- myapi.py
+++ myapi.py
@@ -1,8 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 
 #Create instance of fastAPI
 app = FastAPI()
 
+patients = {
+    1: {
+        "name": "braxton",
+        "age": 4,
+        "trach": "cuffless"
+    }
+}
+
 @app.get("/")
 def index():
     return {"name": "First Data"}
+
+@app.get("/get-patient/{patient_id}")
+def get_patient(patient_id: int = Path(description="The ID of the patient you want to view", gt=0, lt=3)):
+    return patients[patient_id]
